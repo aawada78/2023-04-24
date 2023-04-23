6d6974af873bf24b8675ce6acfbc20fbd34fc074
Assaad Awada <a_awada78@hotmail.com>
2023-04-24 01:26:07 +0200

    First commit

---
+++ src/app/flight-booking/passenger-search/passenger.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Passenger } from './passenger';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PassengerService {
+  private url = 'http://demo.angulararchitects.io/api/passenger';
+  constructor(private http: HttpClient) {
+    console.log('Default Passenger Service', this.url);
+  }
+
+  find(name: string, firstname: string): Observable<Passenger[]> {
+    const params = new HttpParams().set('name', name).set('firstname', firstname);
+    const headers = new HttpHeaders().set('Accept', 'application/json');
+
+    return this.http.get<Passenger[]>(this.url, { params, headers });
+  }
+}
